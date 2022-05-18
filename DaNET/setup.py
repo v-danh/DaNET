1d783f0ac2909b52ef76c580d654ee23791ce3ac
v-danh <vdanhvt2000@gmail.com>
2022-05-19 00:20:41 +0700

    first changes

---
+++ DaNET/setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DaNET Model',
+    version='0.0.1',
+    description='DaNET images for Deep Learning models training',
+    author='VDA',
+    
+    
+    packages=find_packages(),
+)
